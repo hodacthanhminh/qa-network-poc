b48dbc13f7c2f55744468c7ddddb51c8974e1161
hodacthanhminh <hodacthanhminh@gmail.com>
2023-10-04 17:12:34 +0700

    FEAT: standardizeHar

--- cypress/support/harTransformer.ts
+++ cypress/support/harTransformer.ts
@@ -1,19 +1,67 @@
 // types
 import type { Entry } from "har-format";
 
-const addKeyPrefixes = (object: unknown, prefix: string) =>
-  JSON.parse(JSON.stringify(object).replace(/(?<=[{,]{1}\s*")(.+?)(?="\s*:)/gim, `${prefix}-$1`));
+// Check null values
+const isNaN = <T extends string | number | undefined | null>(value: T) =>
+  ["", undefined, -1, null].includes(value);
+// Get rounded integer number
+const formatNumber = (num: number) => Math.round(num);
+// Get valid string
+// https://stackoverflow.com/a/47340986
+const formatString = (str: string) => {
+  if (str.replace(/ /g, "").match(/[\s,"]/)) {
+    return '"' + str.replace(/"/g, '""') + '"';
+  }
 
-const harTransformer = (entry: Entry) => ({
-  ...addKeyPrefixes(entry.timings, "timings"),
-  method: entry.request.method,
-  "request-body-size": entry.request.bodySize,
-  duration: entry.time,
-  url: entry.request.url,
-  "response-status": entry.response.status,
-  "resource-type": entry._resourceType,
-  "content-type": entry.response.content.mimeType,
-  "response-.body-size": entry.response.bodySize,
-});
+  return str;
+};
+
+const harTransformer = (entry: Entry) => {
+  const extractHarEntry = {
+    "timings-blocked": entry.timings.blocked,
+    "timings-dns": entry.timings.dns,
+    "timings-ssl": entry.timings.ssl,
+    "timings-connect": entry.timings.connect,
+    "timings-send": entry.timings.comment,
+    "timings-wait": entry.timings.wait,
+    "timings-receive": entry.timings.receive,
+    method: entry.request.method,
+    "request-body-size": entry.request.bodySize,
+    duration: entry.time,
+    url: entry.request.url,
+    "response-status": entry.response.status,
+    "resource-type": entry._resourceType,
+    "content-type": entry.response.content.mimeType,
+    "response-body-size": entry.response.bodySize,
+  };
+
+  const standardizeHarObject = () =>
+    Object.entries(extractHarEntry).reduce((formattedObject, [key, plainValue]) => {
+      switch (true) {
+        case isNaN(plainValue):
+          return {
+            ...formattedObject,
+            [key]: "NaN",
+          };
+        case typeof plainValue === "number":
+          return {
+            ...formattedObject,
+            [key]: formatNumber(plainValue),
+          };
+        case typeof plainValue === "string":
+          return {
+            ...formattedObject,
+            [key]: formatString(plainValue),
+          };
+        default:
+          return {
+            ...formattedObject,
+            [key]: plainValue,
+          };
+      }
+    }, {});
+
+  return standardizeHarObject();
+};
 
 export default harTransformer;
